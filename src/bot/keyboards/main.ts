45733eb5e97b826834ea1e5bda9e03e90fb00c82
MishaTomash <tomasamihajla@gmail.com>
2026-06-12 18:05:36 +0300

    www

--- src/bot/keyboards/main.ts
+++ src/bot/keyboards/main.ts
@@ -2,8 +2,8 @@ import { Keyboard } from 'grammy';
 
 export const createMainMenu = () => {
   return new Keyboard()
-    .text('📚 Вчити слова').text('📚 Слова по темах').row()
-    .text('📝 Перевірка слів').text('🎯 Міні-тести').row()
+    .text('📚 Вчити слова').text('📝 Перевірка слів').row()
+    .text('📚 Слова по темах').text('🎯 Міні-тести').row()
     .text('💾 Словничок').text('🎓 Курси').row()
     .text('👤 Профіль').text('⚙️ Налаштування').row()
     .text('💎 Premium').text('🏆 Топчик').row()
